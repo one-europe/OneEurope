330314f4ee053ba69be8ff3931478600b78cc18f
One Admin <admin@one-europe.info>
2013-02-15 00:20:51 +0100

    - brief.multiple.php is back in master branch, + excluded some caches via gitignore

--- forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
+++ forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1360766625
+1360885971
 SELECT ban_ip, ban_userid, ban_email, ban_exclude, ban_give_reason, ban_end FROM phpbb_banlist WHERE ban_email = '' AND (ban_userid = 1 OR ban_ip <> '')
 6
 a:0:{}
