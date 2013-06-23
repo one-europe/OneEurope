584d4933df5d3f90001588df790c4e1c065e9250
Valentin Kotov <hello@valentinkotov.de>
2013-06-23 19:44:37 +0200

    - adds target="_blank" to debatingeurope EP vote 2014

--- user/themes/euro/sidebar.elem.de-vote.php
+++ user/themes/euro/sidebar.elem.de-vote.php
@@ -1,7 +1,7 @@
 
 <div class="sidebar-dummy">
 	
-	<a class="vote2014-banner" href="http://www.debatingeurope.eu/vote2014">
+	<a class="vote2014-banner" href="http://www.debatingeurope.eu/vote2014" target="_blank">
 			<img src="<?php Site::out_url('theme');?>/img/ep-vote-2014.png" width="296" alt="DebatingEurope.eu Vote 2014">
 	</a>
 	
