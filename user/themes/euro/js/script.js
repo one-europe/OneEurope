def3bc68c58b41fafd299fea01bb7192109a002e
Dimi Volynskov <iedimitriy@gmail.com>
2013-08-27 22:18:30 +0100

    some more recalculations
    height has changed as now only 4 briefs

--- user/themes/euro/js/script.js
+++ user/themes/euro/js/script.js
@@ -8,7 +8,7 @@
 	$('.list-2').find('img').each(function () {
 		var image = $(this),
 			height = image.height(),
-			marginTopVal = -(Math.ceil(height / 2) - 51);
+			marginTopVal = -(Math.ceil(height / 2) - 65);
 		if (height > 130) image.css({ marginTop: marginTopVal, visibility: 'visible'});
 		else image.css({ height: 130, visibility: 'visible'});
 	});
