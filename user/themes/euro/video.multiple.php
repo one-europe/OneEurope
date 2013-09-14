8e3b03c659c1375a05d8a046f7e261a8dc1e5b9e
Dimi Volynskov <iedimitriy@gmail.com>
2013-09-14 10:13:43 +0100

    video update

--- user/themes/euro/video.multiple.php
+++ user/themes/euro/video.multiple.php
@@ -1,7 +1,12 @@
 <?php echo $theme->display('header'); ?>
 <div id="content" class="videos">
 	<?php foreach ( $videos as $post ) { ?>
-	<h2><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2>
+	<div class="video-item">
+		<a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>">
+			<img class="img" src="<?php Site::out_url( 'theme' ); ?>/img/video-icon.png" height="100" width="160"/>
+		</a>
+		<h2><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2>
+	</div>
 	<?php } ?>
 	<?php if ( $current_page >= 2 || $there_are_more ) { ?>
 		<div class="pagination">
