d0a1f7c495f81d2e6d8513608b48d93328c57d8d
Dimi Volynskov <iedimitriy@gmail.com>
2013-10-29 19:40:15 +0000

    header update

--- user/themes/euro/header.php
+++ user/themes/euro/header.php
@@ -64,13 +64,13 @@
 					
 					<ul class="sf-menu">
 						<li class="menu_1">
-							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/in-brief">The Big Picture</a>
+							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/in-brief">Images</a>
 						</li>
 						<li class="menu_2">
-							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/initiatives">Initiatives</a>
+							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/videos">Videos</a>
 						</li>
 						<li class="menu_3">
-							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/videos">Videos</a>
+							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/initiatives">Initiatives</a>
 						</li>
 						<li class="menu_4">
 							<a rel="section" class="sf-with-ul" href="#">About</a>
@@ -106,6 +106,9 @@
 							</ul>
 						</li>
 						<li class="menu_5">
+							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/join-us">Join us!</a>
+						</li>
+						<li class="menu_6">
 							<?php echo $theme->display ('searchform' ); ?>
 						</li>
 					</ul>
@@ -116,10 +119,10 @@
 					<span>Find us on:</span>
 					<a href="https://facebook.com/OneEurope" class="icon-fb" title="Find us on Facebook" target="_blank"></a>
 					<a href="https://twitter.com/one1europe" class="icon-tw" title="Follow us on Twitter" target="_blank"></a>
+					<a href="http://www.linkedin.com/company/oneeurope" class="icon-in" title="Find us on LinkedIn" target="_blank"></a>
 					<a href="https://plus.google.com/118353934830681553476/posts" class="icon-gp" title="Add us to your circles" target="_blank"></a>
 					<a href="http://pinterest.com/oneeurope" class="icon-pi" title="Find us on Pinterest" target="_blank"></a>
 					<a href="http://www.stumbleupon.com/stumbler/OneEurope" class="icon-st" title="Find us on StumbleUpon" target="_blank"></a>
-					<a href="http://www.linkedin.com/company/oneeurope" class="icon-in" title="Find us on LinkedIn" target="_blank"></a>
 					<a href="http://vk.com/oneeurope" class="icon-vk" title="Find us on VKontakte" target="_blank"></a>
 					<a href="/feeds" class="icon-rs" title="Subscribe via RSS"></a>
 				</div>
--- user/themes/euro/sidebar.php
+++ user/themes/euro/sidebar.php
@@ -23,21 +23,12 @@
 	</section */ ?>
 	
 	<?php echo $theme->display ('sidebar.elem.in-brief'); ?>	
-	
 	<?php echo $theme->display ('sidebar.elem.recent-comments'); ?>
-	
 	<?php echo $theme->display ('sidebar.elem.de-vote'); ?>	
-
 	<?php echo $theme->display ('sidebar.elem.social'); ?>	
-
 	<?php echo $theme->display ('sidebar.elem.most-shared'); ?>	
-	
 	<?php echo $theme->display ('sidebar.elem.debates'); ?>
-	
-	<?php /*echo $theme->display ('sidebar.elem.feedback');*/ ?>
-
 	<?php echo $theme->display ('sidebar.elem.profilepool'); ?>
-
 	<?php echo $theme->display ('sidebar.elem.newsletter'); ?>
 
 
