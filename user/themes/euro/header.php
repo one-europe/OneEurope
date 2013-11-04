a299f49a3d72860c837b420b8bef7459662492ee
Dimi Volynskov <iedimitriy@gmail.com>
2013-11-04 22:33:09 +0000

    header, home and initiatives update

--- user/themes/euro/header.php
+++ user/themes/euro/header.php
@@ -1,46 +1,23 @@
 <!DOCTYPE html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en" prefix="og: http://ogp.me/ns#"> <!--<![endif]-->
-
+<html lang="en" prefix="og: http://ogp.me/ns#">
 	<head>
-				
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title><?php echo Options::get( 'title' ); ?></title>
-		
-		<meta name="Generator" content="Habari" />		
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 		<link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml" />
-		
-		<!-- link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="/atom/1" / -->
-		<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/rss/1" />
-		
-		<!-- link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php URL::out( 'rsd' ); ?>" / -->
-		
 		<link rel="shortcut icon" href="<?php Site::out_url( 'theme' ); ?>/img/favicon.ico" />
-
-		<link href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon.png" rel="apple-touch-icon" />
-		<link href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
-		<link href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
-		<link href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
-		
-		<link rel="stylesheet" media="screen" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/css/style.css?<?php echo date_timestamp_get(date_create()); ?>" />
-				
+		<link rel="stylesheet" media="screen" href="<?php Site::out_url( 'theme' ); ?>/css/style.css?<?php echo date_timestamp_get(date_create()); ?>" />
+		<link rel="apple-touch-icon" href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-72x72.png" sizes="72x72" />
+		<link rel="apple-touch-icon" href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-114x114.png" sizes="114x114" />
+		<link rel="apple-touch-icon" href="<?php Site::out_url( 'theme' ); ?>/img/apple-touch-icon-144x144.png" sizes="144x144" />
 		<?php echo $theme->header(); ?>
-		
 		<script src="<?php Site::out_url( 'theme' ); ?>/js/libs/modernizr-2.5.3.min.js"></script>
 		<script src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fe91cf356685c8e"></script>
-
 	</head>
-
 	<body id="top">
-				
 		<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-		
 		<div id="fb-root"></div>
 		<script>
 			(function(d, s, id) {
@@ -51,17 +28,10 @@
 			  fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
-		
 		<div id="wrapper">
-			
 			<div id="head">
-			
-				<?php /** useful, but out of use <a id="headerbadge" href="<?php Site::out_url( 'habari' ); ?>/europeday"><img src="<?php Site::out_url( 'theme' ); ?>/img/static/badge.png" width="100" height="100" alt="Celebrate Europe Day with a profile banner in your language!" /></a> */ ?>
-
 				<?php if ($home_page) echo '<h1>'; ?><a id="logo" href="<?php Site::out_url( 'habari' ); ?>" title="OneEurope — One Society, One Democracy, One Europe">OneEurope — One Society, One Democracy, One Europe</a><?php if ($home_page) echo '</h1>'; ?>
-					
 				<nav>
-					
 					<ul class="sf-menu">
 						<li class="menu_1">
 							<a rel="section" class="sf-with-ul" href="<?php Site::out_url( 'habari' ); ?>/in-brief">Images</a>
@@ -112,9 +82,7 @@
 							<?php echo $theme->display ('searchform' ); ?>
 						</li>
 					</ul>
-					
 				</nav>
-
 				<div class="sm-buttons">
 					<span>Find us on:</span>
 					<a href="https://facebook.com/OneEurope" class="icon-fb" title="Find us on Facebook" target="_blank"></a>
@@ -126,9 +94,7 @@
 					<a href="http://vk.com/oneeurope" class="icon-vk" title="Find us on VKontakte" target="_blank"></a>
 					<a href="/feeds" class="icon-rs" title="Subscribe via RSS"></a>
 				</div>
-										
 				<div class="clear"></div>
-				
 				<div id="subnav">
 					<div class="debates">Debates:</div>
 					<ul>
--- user/themes/euro/home.php
+++ user/themes/euro/home.php
@@ -1,226 +1,133 @@
 <?php echo $theme->display('header'); ?>
-
-			<div class="featured-content">
-
-				<div class="featured-content-slider">
-			
-					<div id="featured" >
-												
-						<ul class="ui-tabs-nav">
-				        	<?php 
-							// get all posts sys-tagged with 'featured-slideshow' to built the rotating menu
-							$i = 1;
-				        	foreach ( $sliders as $post ) { 
-								if ($post->info->shorttitle) {$title = $post->info->shorttitle; } else { $title = $post->title; };
-					 			?>
-                                  
-							<li class="ui-tabs-nav-item <?php if ( $i == 1 ) { ?>ui-tabs-selected<?php } ?>" id="nav-fragment-<?php echo $i; ?>">
-								<a href="#fragment-<?php echo $i; ?>">
-									<img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />
-									<p><?php echo $title; ?></p>
-								</a>
-							</li>  
-								
-							<?php $i++; } ?>
-							
-						</ul>  
-						
-						<?php // same as above
-						$i = 1;	foreach ( $sliders as $post ) {	?>
-					    <div id="fragment-<?php echo $i; ?>" class="ui-tabs-panel <?php if ( $i != 1 ) { ?>ui-tabs-hide<?php } ?>" style="">
-							<a href="<?php echo $post->permalink; ?>">
-								<?php if ( $i == 1 ) { ?>
-									<img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />  
-								<?php } else { ?>
-									<img src="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />  
-								<?php } ?>
-			        			<div class="info">  
-					    	    	<?php // show caption or excerpt in 155 characters length or 152 + '...'
-									if ($post->info->excerpt) {
-										$string = $post->info->excerpt;
-									} elseif ($post->info->teaser) {
-										$string = $post->info->teaser;
-									} elseif ($post->info->photoinfo) {
-										$string = $post->info->photoinfo;
-									} else { $string = ""; }
-									$text = (strlen($string) > 155) ? substr($string,0,153).'...' : $string;
-									echo $text; ?>
-					    	    </div>  
-							</a>
-					    </div>  
-						<?php $i++; } ?>
-	
-					</div>
-				
-				</div>
-
-				<div class="featured-content-boxes video-block">
-
-					<div>
-						<?php
-							$video = $home_page_video[0];
-							preg_match('/<iframe(.*?)>(.*?)<\/iframe>/si', strip_tags($video->content_fulltext, '<iframe>'), $matches);
-							$iframe = preg_replace(
-								['/width=\"\d+\"/', '/height=\"\d+\"/', '/src=\"(.*?)\"/'],
-								['width="305"', 'height="172"', 'src="${1}?modestbranding=1&rel=0&showinfo=0&controls=0"'],
-								$matches[0]
-							);
-							echo $iframe . '<p><a href="' . $video->permalink . '" title="' . $video->title . '">' . $video->title . '</a></p>';
-						?>
-						<a href="videos" class="all" title="View more videos">View more videos ›</a>
-					</div>
-					
-					<?php /* div class="boxtitle"><span class="inits">Welcome to OneEurope!</span></div>
-	
-						<p>
-							We will fill a crucial gap as a hub for information about organized European civil 
-							action online and independent journalism from a European perspective.
-						</p>
-						<p>
-							We keep an eye on civil initiatives, cross-border debates and online discussions 
-							in order to embed it all into a highly interactive platform.
-						</p>
-						<p>
-							Learn how to <a title="Join us!" href="/join-us">join us on our journey</a>.
-						</p>	
-	
-						<?php /*php
-						foreach($sides as $post):
+	<div class="featured-content">
+		<div class="featured-content-slider">
+			<div id="featured" >
+				<ul class="ui-tabs-nav">
+		        	<?php 
+					// get all posts sys-tagged with 'featured-slideshow' to built the rotating menu
+					$i = 1;
+		        	foreach ( $sliders as $post ) { 
 						if ($post->info->shorttitle) {$title = $post->info->shorttitle; } else { $title = $post->title; };
-						echo '<div class="box">';
-						echo '<h4><a href="' . $post->permalink .'">' . $title . '</a></h4>';
-						echo '<a href="' . $post->permalink .'"><section class="excerpt">' . $post->info->excerpt . '</section></a>';
-						echo '</div>'; ?>
-						<?php endforeach; */?>							
-
-				</div>
-				
-				<div class="clear"></div>
-
-			</div>
-
-			<div style="width: 100%;">
-			
-				<div class="initstitle"><a href="<?php echo Site::out_url( 'habari' ); ?>/in-brief" title="">The Big Picture</a></div>
-
-				<div class="list-2">
-											
-					<?php
-			$i = 0; $j = 1;
-			foreach ($briefsteaser as $brief ) { 	
-
-
-/*				echo '<pre>'; print_r($brief->info->shorttitle); echo '</pre>';
-				exit;*/
-				/* 
-				show only if not currently in the slideshow and if there aren't already two displayed
-				*/	
-				$inslideshow = is_object( Post::get( array( 'vocabulary' => array( 'systags:term' => 'slideshow' ), 'slug' => $brief->slug ) ));
-				if ( $inslideshow == true ) { 
-					if ( $i < $nibblescount ) {
-						$i++;
-						//echo $brief->title . " is featured</br>";
-					}
-				} elseif ( $j <= 4 ) {
-					$j++;
-				?>
-		
-					<div class="list">						
-						<?php if ( $brief->status == Post::status('scheduled') ) { ?>
-							<div class="content-badge scheduled">
-								<span>scheduled</span>
-							</div>
+			 			?>
+					<li class="ui-tabs-nav-item <?php if ( $i == 1 ) { ?>ui-tabs-selected<?php } ?>" id="nav-fragment-<?php echo $i; ?>">
+						<a href="#fragment-<?php echo $i; ?>">
+							<img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />
+							<p><?php echo $title; ?></p>
+						</a>
+					</li>  
+					<?php $i++; } ?>
+				</ul>  
+				<?php // same as above
+				$i = 1;	foreach ( $sliders as $post ) {	?>
+			    <div id="fragment-<?php echo $i; ?>" class="ui-tabs-panel <?php if ( $i != 1 ) { ?>ui-tabs-hide<?php } ?>" style="">
+					<a href="<?php echo $post->permalink; ?>">
+						<?php if ( $i == 1 ) { ?>
+							<img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />  
+						<?php } else { ?>
+							<img src="<?php echo $post->info->photourl; ?>" alt="<?php echo $post->title; ?>" />  
 						<?php } ?>
-			   	    	<a href="<?php echo $brief->permalink; ?>">
-							<div class="img-wrap"><img src="<?php echo $brief->info->photourl; ?>" width="224" /></div>
-			   	    		<h2><?php echo $brief->title; ?></h2>
-						</a>
-					</div>   	    
-
-				<?php /* span class="entry-autor">by <span><?php echo $post->author->displayname; ?></span></span> */ ?>
-			<?php }
-			} ?>
-					
-					<div class="clear"></div>
-					
-				</div>
+	        			<div class="info">  
+			    	    	<?php // show caption or excerpt in 155 characters length or 152 + '...'
+							if ($post->info->excerpt) {
+								$string = $post->info->excerpt;
+							} elseif ($post->info->teaser) {
+								$string = $post->info->teaser;
+							} elseif ($post->info->photoinfo) {
+								$string = $post->info->photoinfo;
+							} else { $string = ""; }
+							$text = (strlen($string) > 155) ? substr($string,0,153).'...' : $string;
+							echo $text; ?>
+			    	    </div>  
+					</a>
+			    </div>  
+				<?php $i++; } ?>
 			</div>
-
-			<div id="content" class="home">
-
-				<!-- last actions -->				
-				
-				<?php /*div class="tile-depth-1 tile-thumbs">
-
-					<?php $feats = Posts::get( array( 'content_type' => 'action', 'status' => Post::status( 'published' ), 'limit' => 4 ) );
-					foreach ($feats as $post ) { ?>
-						
-						<a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>" class="tile-depth-2-v1">
-					
-							<section>
-
-									<?php if ($post->info->photourl) { ?>
-										<img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" />
-									<?php } ?>
-									<h2><?php echo $post->title_out; ?></h2>
-									<span class="entry-tags">
-										<time datetime="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}'); ?>"><?php echo $post->pubdate->text_format('<span>{M}</span> <span>{d}</span>, <span>{Y}</span>'); ?></time>
-										<!-- a class="entry-comments" href="<?php echo $post->url; ?>#disqus_thread" data-disqus-identifier="<?php echo $page; ?><?php echo $post->permalink; ?>">Comments</a -->
-									</span>
-	
-							</section>
-						
-						</a>
-					<?php } ?>
-								
-
-				</div */ ?>	
-
-			
-				<div class="tile-depth-1 list-1">
-					<?php 
-					foreach ($posts as $post ) {
-						if ($post->content_type != 17) {
-
-						?>						
-							<div class="list">
-								<a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php if ( $post->info->photoinfo ) { echo $post->info->photoinfo; } else { echo $post->title; } ?>" height="100" width="160"/></a>
-								<header><h2><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2></header>
-								<article class="body"><?php if ( $post->info->excerpt ) { echo $post->info->excerpt; } else { echo $post->content_out; }?></article>
-								<footer>
-									<span class="entry-tags">
-								        <?php if ( $show_author && $post->typename == 'article' ) { ?>
-											<span class="entry-autor">
-												<?php if ( $post->info->origauthor ) { ?>
-													<a href="<?php if ( $post->info->origprofile ) { echo $post->info->origprofile; } else { echo $post->info->origsource; } ?>" title="Portrait"><span><?php echo $post->info->origauthor; ?></span></a>
-												<?php } elseif ($post->info->author) { ?>
-													<?php $publisher = Post::get(array( 'all:info' => array( 'user' => $post->info->author ) ) );?>
-													<a href="<?php echo $publisher->permalink; ?>" title="Portrait"><span><?php echo User::get($post->info->author)->displayname; ?></span></a>
-												<?php } else { 
-														if (is_object(Post::get(array( 'all:info' => array( 'user' => $post->author->id ) )))) { 
-															$publisher = Post::get(array( 'all:info' => array( 'user' => $post->author->id ) ) );?>
-													<a href="<?php echo $publisher->permalink; ?>" title="Portrait"><span><?php echo $post->author->displayname; ?></span></a>
-														<?php } else { ?>
-													<span><?php echo $post->author->displayname; ?></span>
-														<?php } ?>
+		</div>
+		<div class="featured-content-boxes video-block">
+			<div>
+				<?php
+					$video = $home_page_video[0];
+					preg_match('/<iframe(.*?)>(.*?)<\/iframe>/si', strip_tags($video->content_fulltext, '<iframe>'), $matches);
+					$iframe = preg_replace(
+						['/width=\"\d+\"/', '/height=\"\d+\"/', '/src=\"(.*?)\"/'],
+						['width="305"', 'height="172"', 'src="${1}?modestbranding=1&amp;rel=0&amp;showinfo=0&amp;controls=0"'],
+						$matches[0]
+					);
+					echo $iframe . '<p><a href="' . $video->permalink . '" title="' . $video->title . '">' . $video->title . '</a></p>';
+				?>
+				<a href="videos" class="all" title="View more videos">View more videos ›</a>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div style="width: 100%;">
+		<div class="initstitle"><a href="<?php echo Site::out_url( 'habari' ); ?>/in-brief" title="">The Big Picture</a></div>
+		<div class="list-2">
+			<?php
+				$i = 0; $j = 1;
+				foreach ($briefsteaser as $brief ) {	
+					// show only if not currently in the slideshow and if there aren't already two displayed
+					$inslideshow = is_object( Post::get( array( 'vocabulary' => array( 'systags:term' => 'slideshow' ), 'slug' => $brief->slug ) ));
+					if ( $inslideshow == true ) { 
+						if ( $i < $nibblescount ) $i++;
+					} elseif ( $j <= 4 ) {
+						$j++;
+			?>
+				<div class="list">
+				<?php if ( $brief->status == Post::status('scheduled') ) { ?>
+					<div class="content-badge scheduled">
+						<span>scheduled</span>
+					</div>
+				<?php } ?>
+					<a href="<?php echo $brief->permalink; ?>">
+					<div class="img-wrap"><img src="<?php echo $brief->info->photourl; ?>" width="224" alt="" /></div>
+						<h2><?php echo $brief->title; ?></h2>
+					</a>
+				</div>
+			<?php } } ?>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div id="content" class="home">
+		<div class="tile-depth-1 list-1">
+			<?php 
+			foreach ($posts as $post ) {
+				if ($post->content_type != 17) { ?>
+					<div class="list">
+						<a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><img src="<?php Site::out_url( 'theme' ); ?>/img/grey.gif" data-original="<?php echo $post->info->photourl; ?>" alt="<?php if ( $post->info->photoinfo ) { echo $post->info->photoinfo; } else { echo $post->title; } ?>" height="100" width="160"/></a>
+						<header><h2><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2></header>
+						<article class="body"><?php if ( $post->info->excerpt ) { echo $post->info->excerpt; } else { echo $post->content_out; }?></article>
+						<footer>
+							<span class="entry-tags">
+						        <?php if ( $show_author && $post->typename == 'article' ) { ?>
+									<span class="entry-autor">
+										<?php if ( $post->info->origauthor ) { ?>
+											<a href="<?php if ( $post->info->origprofile ) { echo $post->info->origprofile; } else { echo $post->info->origsource; } ?>" title="Portrait"><span><?php echo $post->info->origauthor; ?></span></a>
+										<?php } elseif ($post->info->author) { ?>
+											<?php $publisher = Post::get(array( 'all:info' => array( 'user' => $post->info->author ) ) );?>
+											<a href="<?php echo $publisher->permalink; ?>" title="Portrait"><span><?php echo User::get($post->info->author)->displayname; ?></span></a>
+										<?php } else { 
+												if (is_object(Post::get(array( 'all:info' => array( 'user' => $post->author->id ) )))) { 
+													$publisher = Post::get(array( 'all:info' => array( 'user' => $post->author->id ) ) );?>
+											<a href="<?php echo $publisher->permalink; ?>" title="Portrait"><span><?php echo $post->author->displayname; ?></span></a>
+												<?php } else { ?>
+											<span><?php echo $post->author->displayname; ?></span>
 												<?php } ?>
-											</span>
 										<?php } ?>
-								        on <time datetime="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}'); ?>"><?php echo $post->pubdate->text_format('<span>{M}</span> <span>{d}</span>, <span>{Y}</span>'); ?></time>
 									</span>
-	        						<a class="alignright entry-comments" href="<?php echo $post->permalink ?>#disqus_thread">Comments</a>
-								</footer>
-							</div>
-					<?php }} ?>
-				</div>
-		
-				<div class="pagination">
-					<?php echo $theme->prev_page_link( '&laquo;' . _t('Newer Posts') ); ?>
-					<?php echo $theme->page_selector( null, array( 'leftSide' => 20, 'rightSide' => 20 ) ); ?>
-					<?php echo $theme->next_page_link( _t('&nbsp;Older Posts') . '&raquo;' ); ?>
-				</div>
-		
-			</div>
-
+								<?php } ?>
+						        on <time datetime="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}'); ?>"><?php echo $post->pubdate->text_format('<span>{M}</span> <span>{d}</span>, <span>{Y}</span>'); ?></time>
+							</span>
+    						<a class="alignright entry-comments" href="<?php echo $post->permalink ?>#disqus_thread">Comments</a>
+						</footer>
+					</div>
+			<?php }} ?>
+		</div>
+		<div class="pagination">
+			<?php echo $theme->prev_page_link( '&laquo;' . _t('Newer Posts') ); ?>
+			<?php echo $theme->page_selector( null, array( 'leftSide' => 20, 'rightSide' => 20 ) ); ?>
+			<?php echo $theme->next_page_link( _t('&nbsp;Older Posts') . '&raquo;' ); ?>
+		</div>
+	</div>
 <?php echo $theme->display ('sidebar'); ?>
 <?php echo $theme->display ('footer'); ?>
