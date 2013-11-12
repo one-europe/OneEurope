a3717c0bddb1e45d254b96a0b25386a8b9e9f9bf
Dimi Volynskov <iedimitriy@gmail.com>
2013-11-12 22:22:30 +0000

    remove thinkyoung logo and address

--- user/themes/euro/page.contact.php
+++ user/themes/euro/page.contact.php
@@ -1,84 +1,53 @@
 <?php echo $theme->display('header'); ?>
-
-
-			<div class="submenu">
-				<ul>
-					<li><a href="/about">About</a></li>
-					<li><a href="/join-us">Join us</a></li>
-					<li><a href="/become-a-patron">Become a Patron</a></li>
-					<li><a href="/contact"><b>Contact</b></a></li>
-					<li class="clear"></li>
-				</ul>
-			</div>
-			
-			
-			<div class="page">
-								
-				<h1>Contact</h1><br/>
-			
-				<p>We are keen to meet you, but please get in touch first.</p>
-			
-			
-				<section id="contact-email">
-			
-					<h3>E-Mail</h3>
-				
-					<p>For general information, please contact 
-				
-						<script type="text/javascript">
-							emailE='one-europe.info'
-							emailE=('info' + '@' + emailE)
-							document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
-						</script><noscript>
-						    <em>[Email address protected by JavaScript. Enable JavaScript to see it]<em>
-						</noscript>.<br/>
-
-						For public relations, please contact Ivan at 
-				 			<script type="text/javascript">
-				 				emailE='one-europe.info'
-				 				emailE=('ivan' + '@' + emailE)
-				 				document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
-				 			</script><noscript>
-				 			    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
-				 			</noscript>.
-						<br/>
-						For fundraising, please send an email to Cherian at 
-							<script type="text/javascript">
-								emailE='one-europe.info'
-								emailE=('cherian' + '@' + emailE)
-								document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
-							</script><noscript>
-							    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
-							</noscript>.
-						<br/>
-						For IT, please email Valentin at 
-							<script type="text/javascript">
-								emailE='one-europe.info'
-								emailE=('valentin' + '@' + emailE)
-								document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
-							</script><noscript>
-							    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
-							</noscript>.
-					</p>
-		
-				</section>
-		
-				<section id="contact-office">
-					
-					<h3>Brussels Office</h3>
-
-					<p>
-						OneEurope,<br/> 
-						Place du Luxembourg 6,<br/> 
-						1050 Bruxelles,<br/> 
-						Belgium
-					</p>
-			
-				</section>
-				
-				<div id="map_canvas" style="width:100%; height:300px"></div>
-				
-			</div>
-			
+<div class="submenu">
+	<ul>
+		<li><a href="/about">About</a></li>
+		<li><a href="/join-us">Join us</a></li>
+		<li><a href="/become-a-patron">Become a Patron</a></li>
+		<li><a href="/contact"><b>Contact</b></a></li>
+		<li class="clear"></li>
+	</ul>
+</div>
+<div class="page">
+	<h1>Contact</h1><br/>
+	<p>We are keen to meet you, but please get in touch first.</p>
+	<section id="contact-email">
+		<h3>E-Mail</h3>
+		<p>For general information, please contact 
+			<script type="text/javascript">
+				emailE='one-europe.info'
+				emailE=('info' + '@' + emailE)
+				document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+			</script><noscript>
+			    <em>[Email address protected by JavaScript. Enable JavaScript to see it]<em>
+			</noscript>.<br/>
+			For public relations, please contact Ivan at 
+	 			<script type="text/javascript">
+	 				emailE='one-europe.info'
+	 				emailE=('ivan' + '@' + emailE)
+	 				document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+	 			</script><noscript>
+	 			    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
+	 			</noscript>.
+			<br/>
+			For fundraising, please send an email to Cherian at 
+				<script type="text/javascript">
+					emailE='one-europe.info'
+					emailE=('cherian' + '@' + emailE)
+					document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+				</script><noscript>
+				    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
+				</noscript>.
+			<br/>
+			For IT, please email Valentin at 
+				<script type="text/javascript">
+					emailE='one-europe.info'
+					emailE=('valentin' + '@' + emailE)
+					document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+				</script><noscript>
+				    <em>[Email address protected by JavaScript. Enable JavaScript to see it.]<em>
+				</noscript>.
+		</p>
+	</section>
+</div>
 <?php echo $theme->display ('footer'); ?>
-
