73f1fa14369d7626e8b0773d315ac3dbb36bbeec
Dimi Volynskov <iedimitriy@gmail.com>
2014-02-26 21:33:14 +0000

    replace with live keys
    crowdfunding-page

--- user/themes/euro/charge.php
+++ user/themes/euro/charge.php
@@ -4,8 +4,8 @@ if ($_POST) {
 	require_once('./lib/Stripe.php');
 	
 	$stripe = array(
-		"secret_key"      => "sk_test_C0J7bbsVhtfCzWkQ9r4uQFtD",
-		"publishable_key" => "pk_test_XaplsFD7NLA1u19mjKVM1r6Q"
+		"secret_key"      => "sk_live_CTroOsJmZJJPGEfdQtuf45kh",
+		"publishable_key" => "pk_live_d7uVopGCxMtSVQS60zuJtIAF"
 	);
 	
 	try {
