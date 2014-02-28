a39b10374d145e3b58c5fd86d66a784621dd9606
Dimi Volynskov <iedimitriy@gmail.com>
2014-02-28 07:30:20 +0000

    replace keys in config

--- user/themes/euro/config.php
+++ user/themes/euro/config.php
@@ -1,8 +1,8 @@
 <?php
   require_once(HABARI_PATH . '/user/themes/euro/lib/Stripe.php');
   $stripe = array(
-    "secret_key"      => "sk_test_C0J7bbsVhtfCzWkQ9r4uQFtD",
-    "publishable_key" => "pk_test_XaplsFD7NLA1u19mjKVM1r6Q"
+    "secret_key"      => "sk_live_CTroOsJmZJJPGEfdQtuf45kh",
+    "publishable_key" => "pk_live_d7uVopGCxMtSVQS60zuJtIAF"
   );
   Stripe::setApiKey($stripe['secret_key']);
 ?>
