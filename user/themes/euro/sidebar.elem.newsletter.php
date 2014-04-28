85a6c7b0cd55a173bb157782bd8718eca3dde056
Dimi Volynskov <iedimitriy@gmail.com>
2014-04-28 07:28:06 +0100

    newsletter update
    update newsletter box and put it at the top on homepage

--- user/themes/euro/sidebar.elem.newsletter.php
+++ user/themes/euro/sidebar.elem.newsletter.php
@@ -1,7 +1,7 @@
-<section>
+<section class="newsletter-sidebar">
 	<div class="h"><span>Newsletter</span></div>
 	<!-- Begin MailChimp Signup Form -->
-	<div id="mc_embed_signup" class="newsletter-sidebar">
+	<div id="mc_embed_signup">
 		<form action="http://one-europe.us5.list-manage1.com/subscribe/post?u=fad146f9810377d640e431dfd&amp;id=e147a25731" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			<label for="mce-EMAIL">Subscribe to our newsletter:</label>
 			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your email" required>
--- user/themes/euro/sidebar.php
+++ user/themes/euro/sidebar.php
@@ -3,25 +3,9 @@
 	<div class="welcome">
 		<a href="/about" title="We incite cross-border debate for the emerging European society"></a>
 	</div>
-
 	<?php Plugins::act( 'theme_sidebar_top' ); ?>
 
-	<?php /* section class="">
-		<div class="h"><span>{recent activity}</span></div>
-		
-		<div>Recent/trending content (upcoming or happening event, initiative,...)<br/>
-			TODO: Create a slider.</div>
-		
-	</section */?>
-	
-	
-	<?php /* section class="disqusthreads">
-		<div class="h"><span class="dsq-widget-title">Most Commented</span></div>
-		<div id="popularthreads" class="dsq-widget">
-			<script type="text/javascript" src="http://oneeurope.disqus.com/popular_threads_widget.js?num_items=5"></script>
-		</div>
-	</section */ ?>
-	
+	<?php echo $theme->display ('sidebar.elem.newsletter'); ?>
 	<?php echo $theme->display ('sidebar.elem.in-brief'); ?>	
 	<?php echo $theme->display ('sidebar.elem.recent-comments'); ?>
 	<?php echo $theme->display ('sidebar.elem.de-vote'); ?>	
@@ -29,7 +13,6 @@
 	<?php echo $theme->display ('sidebar.elem.most-shared'); ?>	
 	<?php echo $theme->display ('sidebar.elem.debates'); ?>
 	<?php echo $theme->display ('sidebar.elem.profilepool'); ?>
-	<?php echo $theme->display ('sidebar.elem.newsletter'); ?>
 
 
 	<?php /* section id="subscribe">	
