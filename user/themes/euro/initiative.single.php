341b4b7b9cdafc04d9a63d56f3d2f744bea5bd00
Dimi Volynskov <iedimitriy@gmail.com>
2014-04-28 08:22:09 +0100

    follow us update
    put follow us at the bottom
    and update newsletter

--- user/themes/euro/initiative.single.php
+++ user/themes/euro/initiative.single.php
@@ -55,9 +55,20 @@
 					<?php } ?>
 			<?php } ?>														
 		</div>
-		<div class="clear"></br></br></div>
-		<div class="disqus">
+		<div class="clear"></div>
+		<aside class="disqus" style="margin-bottom: 15px;">
 			<?php $theme->comments( $post ); ?>
+		</aside>
+		<div class="sm-buttons at-the-bottom">
+			<span>Find us on:</span>
+			<a href="https://facebook.com/OneEurope" class="icon-fb" title="Find us on Facebook" target="_blank"></a>
+			<a href="https://twitter.com/one1europe" class="icon-tw" title="Follow us on Twitter" target="_blank"></a>
+			<a href="http://www.linkedin.com/company/oneeurope" class="icon-in" title="Find us on LinkedIn" target="_blank"></a>
+			<a href="https://plus.google.com/118353934830681553476/posts" class="icon-gp" title="Add us to your circles" target="_blank"></a>
+			<a href="http://pinterest.com/oneeurope" class="icon-pi" title="Find us on Pinterest" target="_blank"></a>
+			<a href="http://www.stumbleupon.com/stumbler/OneEurope" class="icon-st" title="Find us on StumbleUpon" target="_blank"></a>
+			<a href="http://vk.com/oneeurope" class="icon-vk" title="Find us on VKontakte" target="_blank"></a>
+			<a href="/feeds" class="icon-rs" title="Subscribe via RSS"></a>
 		</div>
 	</div>
 <?php echo $theme->display('sidebar.initiative.right'); ?>
--- user/themes/euro/page.single.php
+++ user/themes/euro/page.single.php
@@ -12,6 +12,17 @@
 		</footer>
 	</article>
 	<aside class="disqus"><?php $theme->comments($post); ?></aside>
+	<div class="sm-buttons at-the-bottom">
+		<span>Find us on:</span>
+		<a href="https://facebook.com/OneEurope" class="icon-fb" title="Find us on Facebook" target="_blank"></a>
+		<a href="https://twitter.com/one1europe" class="icon-tw" title="Follow us on Twitter" target="_blank"></a>
+		<a href="http://www.linkedin.com/company/oneeurope" class="icon-in" title="Find us on LinkedIn" target="_blank"></a>
+		<a href="https://plus.google.com/118353934830681553476/posts" class="icon-gp" title="Add us to your circles" target="_blank"></a>
+		<a href="http://pinterest.com/oneeurope" class="icon-pi" title="Find us on Pinterest" target="_blank"></a>
+		<a href="http://www.stumbleupon.com/stumbler/OneEurope" class="icon-st" title="Find us on StumbleUpon" target="_blank"></a>
+		<a href="http://vk.com/oneeurope" class="icon-vk" title="Find us on VKontakte" target="_blank"></a>
+		<a href="/feeds" class="icon-rs" title="Subscribe via RSS"></a>
+	</div>
 </div>
 <?php echo $theme->display('sidebar'); ?>
 <?php echo $theme->display('footer'); ?>
