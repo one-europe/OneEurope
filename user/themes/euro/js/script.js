a81aca68600ad04e6aae4a126cb06a6f54e6e3eb
Dimi Volynskov <iedimitriy@gmail.com>
2014-05-18 11:06:19 +0100

    scale on ipad, iphone

--- user/themes/euro/js/script.js
+++ user/themes/euro/js/script.js
@@ -249,3 +249,13 @@ if (flattrButton && flattrButton.length) {
 	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(po, s);
 })();
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+}
