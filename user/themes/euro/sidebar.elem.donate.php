67aa6b1a7bfa0699860e4440aaf6d96dfb837865
Dimi <volynskov@hotmail.com>
2014-10-07 08:03:33 +0100

    donate box
    - add donate box in sidebar

---
+++ user/themes/euro/sidebar.elem.donate.php
@@ -0,0 +1,6 @@
+<section class="side-block">
+	<a class="top-link" href="#">Donate</a>
+	<a class="donate-text" href="<?php Site::out_url( 'habari' ); ?>/donate">
+		<span>It's your time to act: </span>help us make OneEurope bigger and better!
+	</a>
+</section>
--- user/themes/euro/sidebar.php
+++ user/themes/euro/sidebar.php
@@ -2,6 +2,7 @@
 	<?php echo $theme->display('sidebar.elem.yearbook'); ?>
 	<?php echo $theme->display('sidebar.elem.welcome'); ?>
 	<?php echo $theme->display('sidebar.elem.newsletter'); ?>
+	<?php echo $theme->display('sidebar.elem.donate'); ?>
 	<?php echo $theme->display('sidebar.elem.initiatives'); ?>
 	<?php echo $theme->display('sidebar.elem.social'); ?>
 	<?php echo $theme->display('sidebar.elem.most-shared'); ?>
