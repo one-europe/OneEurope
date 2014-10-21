4ef7aae4a6f98b328c8f121c720fea19e00a6437
Dimi <volynskov@hotmail.com>
2014-10-21 22:51:06 +0100

    404 text update

--- user/themes/euro/404.php
+++ user/themes/euro/404.php
@@ -3,8 +3,7 @@
 <article class="full">
 	<h1>Page Not Found</h1>
 	<div class="post-content">
-		<p>Sorry, the page you're looking for could not be found..</p>
-		<p>Please, try one of the following pages: <a href="<?php Site::out_url( 'habari' ); ?>">home page</a></p>
+		<p>Sorry, the page you're looking for could not be found.</p>
 	</div>
 </article>
 <?php echo $theme->display('footer'); ?>
