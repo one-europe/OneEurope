82fcef1c70e5834c4544511857e5e98113ce6cbf
Dimi <volynskov@hotmail.com>
2014-10-21 22:43:49 +0100

    404 page and new webmasters file
    - update 404 page;
    - update google webmasters file.

--- user/themes/euro/404.php
+++ user/themes/euro/404.php
@@ -1,14 +1,9 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-
-<?php $theme->display ( 'header' ); ?>
-
-<!--begin content-->
-
-	<div class="centered">
-		<h1>What you were looking for couldn't be found.</h1>
-		<h1>Please check your spelling, try the search function or otherwise maybe go to the <a href="http://one-europe.info/">› home page</a>.</h1>
+<?php echo $theme->display('header'); ?>
+<article class="full">
+	<h1>What you were looking for couldn't be found</h1>
+	<div class="post-content">
+		<p>Please check your spelling, try the search function or otherwise maybe go to the <a href="http://one-europe.info/">› home page</a>.</p>
 	</div>
-
-<?php } ?>
-	
-<?php $theme->display ( 'footer' ); ?>
+</article>
+<?php echo $theme->display('footer'); ?>
