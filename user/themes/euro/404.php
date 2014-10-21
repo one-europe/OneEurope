8801ed91f76db650a346de134fc1ec10cdef4ac3
Dimi <volynskov@hotmail.com>
2014-10-21 22:49:36 +0100

    404 text update

--- user/themes/euro/404.php
+++ user/themes/euro/404.php
@@ -1,9 +1,10 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 <?php echo $theme->display('header'); ?>
 <article class="full">
-	<h1>What you were looking for couldn't be found</h1>
+	<h1>Page Not Found</h1>
 	<div class="post-content">
-		<p>Please check your spelling, try the search function or otherwise maybe go to the <a href="http://one-europe.info/">› home page</a>.</p>
+		<p>Sorry, the page you're looking for could not be found..</p>
+		<p>Please, try one of the following pages: <a href="<?php Site::out_url( 'habari' ); ?>">home page</a></p>
 	</div>
 </article>
 <?php echo $theme->display('footer'); ?>
