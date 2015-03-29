5407120bf3a8b7b88bceb05565f1efac4e609864
Dmitrij <volynskov@hotmail.com>
2015-03-29 20:17:59 +0100

    instagram link for bottom social

--- user/themes/euro/social.buttons-bottom.php
+++ user/themes/euro/social.buttons-bottom.php
@@ -5,7 +5,7 @@
 	<a href="http://www.linkedin.com/company/oneeurope" class="icon-in" title="Find us on LinkedIn" target="_blank"></a>
 	<a href="https://plus.google.com/118353934830681553476/posts" class="icon-gp" title="Add us to your circles" target="_blank"></a>
 	<a href="http://pinterest.com/oneeurope" class="icon-pi" title="Find us on Pinterest" target="_blank"></a>
-	<a href="http://www.stumbleupon.com/stumbler/OneEurope" class="icon-st" title="Find us on StumbleUpon" target="_blank"></a>
+	<a href="https://instagram.com/oneeurope/" class="icon-st" title="Find us on Instagram" target="_blank"></a>
 	<a href="http://vk.com/oneeurope" class="icon-vk" title="Find us on VKontakte" target="_blank"></a>
 	<a href="/feeds" class="icon-rs" title="Subscribe via RSS"></a>
 </div>
